21091ae7588af6406a0b9fd408186504f0eb9d42
ohshitnima <42464662+ohshitnima@users.noreply.github.com>
2019-09-13 23:49:59 +0800

    Update 图站放大li.user.js

--- 图站放大li.user.js
+++ 图站放大li.user.js
@@ -14,13 +14,18 @@
 (function() {
     'use strict';
     var scale_wswm = 2;
+    var width_wswm=150*scale_wswm;
     var wswm$=jQuery.noConflict();
-    wswm$("div.inner").css({"width": "320px","height": "300px"});
-    wswm$("ul#post-list-posts li").css("width","310px");
+    wswm$("div.inner").css({"width":width_wswm+20+'px',"height": width_wswm+'px'});
+    wswm$("ul#post-list-posts li").css("width",width_wswm+10+'px');
     wswm$("a.thumb img").each(function(){
-        wswm$(this).css("width",wswm$(this).width()*2);
-         wswm$(this).css("height",wswm$(this).height()*2);
+        wswm$(this).css("width",wswm$(this).width()*scale_wswm);
+        wswm$(this).css("height",wswm$(this).height()*scale_wswm);
                                         });
-    wswm$("a").attr("target","_blank");
+    wswm$("a.directlink").each(function(){
+        wswm$(this).css("height",wswm$(this).height()*scale_wswm);
+        wswm$(this).attr("target","_blank");
+        wswm$(this).children(0).css("font-size",10*scale_wswm+"px");
+    })
     // Your code here...
 })();
